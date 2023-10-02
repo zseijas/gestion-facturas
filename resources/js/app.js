6da10d0ea1cf19977fa382dc1ade6c42c86638bf
zseijas <zseijas.personal@gmail.com>
2023-10-02 15:39:51 -0400

    advances vue2 integration

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,36 +4,17 @@
  * building robust, powerful web applications using Vue and Laravel.
  */
 import Vue from 'vue'
-import ExampleComponent from  './components/ExampleComponent'
-import { createApp } from 'vue'
-/* import router from './router.js'
-import store from './store/index'
-import utils from './helpers/utilities'
-import i18n from './plugins/i18n' */
-// import swal from 'sweetalert'
+// import { createApp } from 'vue'
 
 require('./bootstrap')
-
-//Vue.prototype.$utils = utils
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
+window.hub = new Vue()
 
-// window.i18n = i18n
+new Vue({
 
-/* new Vue({
-  router,
-  store,
-  i18n,
-  swal,
-}).$mount('#app') */
-
-
-createApp({
-  components: {
-		ExampleComponent,
-	}
-}).mount('#app');
+  }).$mount('#app')
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,10 +3,10 @@
 
 <head>
     <title>Crater - Self Hosted Invoicing Platform</title>
+    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600&display=swap" rel="stylesheet">
     {{-- <script src="/assets/js/pace/pace.js"></script>
     <link href="{{mix("/assets/css/crater.css")}}" rel="stylesheet" type="text/css">
     <meta name="viewport" content="width=device-width,initial-scale=1">
-    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600&display=swap" rel="stylesheet">
     <link rel="apple-touch-icon" sizes="180x180" href="/assets/img/favicons/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="/assets/img/favicons/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="/assets/img/favicons/favicon-16x16.png">
